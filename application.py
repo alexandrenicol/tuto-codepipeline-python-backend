a1b1fb3774fb078d03e48473e68baab3d0c672f3
Alexandre Nicol <nicol.alexandre1@gmail.com>
2018-09-07 10:18:54 +0100

    clean up

--- application.py
+++ application.py
@@ -1,40 +1,9 @@
-import logging
-import logging.handlers
-
 from wsgiref.simple_server import make_server
 
-
-# Create logger
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-
-# Handler 
-LOG_FILE = '/opt/python/log/sample-app.log'
-handler = logging.handlers.RotatingFileHandler(LOG_FILE, maxBytes=1048576, backupCount=5)
-handler.setLevel(logging.INFO)
-
-# Formatter
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-# Add Formatter to Handler
-handler.setFormatter(formatter)
-
-# add Handler to Logger
-logger.addHandler(handler)
-
 welcome = """
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<!DOCTYPE html>
 <html>
 <head>
-  <!--
-    Copyright 2012 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-
-    Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
-
-        http://aws.Amazon/apache2.0/
-
-    or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
-  -->
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
   <title>Welcome</title>
   <style>
@@ -133,19 +102,20 @@ welcome = """
 def application(environ, start_response):
     path    = environ['PATH_INFO']
     method  = environ['REQUEST_METHOD']
-    if method == 'POST':
-        try:
-            if path == '/':
-                request_body_size = int(environ['CONTENT_LENGTH'])
-                request_body = environ['wsgi.input'].read(request_body_size).decode()
-                logger.info("Received message: %s" % request_body)
-            elif path == '/scheduled':
-                logger.info("Received task %s scheduled at %s", environ['HTTP_X_AWS_SQSD_TASKNAME'], environ['HTTP_X_AWS_SQSD_SCHEDULED_AT'])
-        except (TypeError, ValueError):
-            logger.warning('Error retrieving request body for async work.')
-        response = ''
-    else:
-        response = welcome
+    # if method == 'POST':
+    #     try:
+    #         if path == '/':
+    #             request_body_size = int(environ['CONTENT_LENGTH'])
+    #             request_body = environ['wsgi.input'].read(request_body_size).decode()
+    #             logger.info("Received message: %s" % request_body)
+    #         elif path == '/scheduled':
+    #             logger.info("Received task %s scheduled at %s", environ['HTTP_X_AWS_SQSD_TASKNAME'], environ['HTTP_X_AWS_SQSD_SCHEDULED_AT'])
+    #     except (TypeError, ValueError):
+    #         logger.warning('Error retrieving request body for async work.')
+    #     response = ''
+    # else:
+    #     response = welcome
+    response = welcome
     status = '200 OK'
     headers = [('Content-type', 'text/html')]
 
